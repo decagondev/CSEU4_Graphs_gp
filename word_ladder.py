a6d01eaec587e8b16415ee8c05e075f1d86e7772
Tom Tarpey <tomtarpeydev@gmail.com>
2020-05-12 10:14:39 +0100

    finished solution

--- word_ladder.py
+++ word_ladder.py
@@ -11,26 +11,59 @@
 - thinking about how to get all edges (???)
 
 """
+from queue import Queue
 
-# open the words.txt file
-f = open("words.txt", "r")
+f = open('words.txt', 'r')
 words = f.read().split("\n")
 f.close()
 
 word_set = set()
 for word in words:
-    word_set.add(word.lower())
-
-# list of letters
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'w', 'x', 'y', 'z']
+      word_set.add(word.lower())
 
 def get_neighbors(word):
     neighbors = []
-    # TODO: the logic for getting neighbors
+    string_word = list(word)
+    for i in range(len(string_word)):
+        for letter in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']:
+            temp_word = list(string_word)
+            temp_word[i] = letter
+            w = "".join(temp_word)
+            if w != word and w in word_set:
+                neighbors.append(w)
     return neighbors
 
+
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
+
+
+
 def find_word_ladder(begin_word, end_word):
-    pass
+    visited = set()
+    q = Queue()
+    q.enqueue( [begin_word] )
+    while q.size() > 0:
+        path = q.dequeue()
+        v = path[-1]
+        if v not in visited:
+            visited.add(v)
+            if v == end_word:
+                return path
+            for neighbor in get_neighbors(v):
+                path_copy = list(path)
+                path_copy.append(neighbor)
+                q.enqueue(path_copy)
 
 
 print(find_word_ladder("sail", "boat"))
