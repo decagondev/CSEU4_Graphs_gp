6531280ed0eaec8d834b911654aa120b540cab78
Tom Tarpey <tomtarpeydev@gmail.com>
2020-05-13 11:34:47 +0100

    flattening theory

---
+++ flattening.py
@@ -0,0 +1,20 @@
+arr = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16]
+]
+arr2 = []
+for i in range(1, 17):
+    arr2.append(i)
+
+print(arr)
+print(arr2)
+print(arr[0][1])
+
+# x + y * width
+print(arr2[1 + 0 * 4])
+
+
+
+
