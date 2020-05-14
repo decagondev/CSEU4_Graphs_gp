2dbf04ac276d855a8dfe0e66b882f77010a42776
Tom Tarpey <tomtarpeydev@gmail.com>
2020-05-14 10:12:33 +0100

    social graph linear and collisions

--- social_graph.py
+++ social_graph.py
@@ -1,5 +1,6 @@
 import random
 from queue import Queue
+import time
 
 class User:
     def __init__(self, name):
@@ -16,12 +17,15 @@ class SocialGraph:
         Creates a bi-directional friendship
         """
         if user_id == friend_id:
-            print("WARNING: You cannot be friends with yourself")
+            # print("WARNING: You cannot be friends with yourself")
+            return False
         elif friend_id in self.friendships[user_id] or user_id in self.friendships[friend_id]:
-            print("WARNING: Friendship already exists")
+            # print("WARNING: Friendship already exists")
+            return False
         else:
             self.friendships[user_id].add(friend_id)
             self.friendships[friend_id].add(user_id)
+            return True
 
     def add_user(self, name):
         """
@@ -68,6 +72,38 @@ class SocialGraph:
             friendship = possible_friendships[i]
             self.add_friendship(friendship[0], friendship[1])
 
+    def populate_graph_linear(self, num_users, avg_friendships):
+        """
+        Takes a number of users and an average number of friendships
+        as arguments
+        Creates that number of users and a randomly distributed friendships
+        between those users.
+        The number of users must be greater than the average number of friendships.
+        """
+        # Reset graph
+        self.last_id = 0
+        self.users = {}
+        self.friendships = {}
+        # !!!! IMPLEMENT ME
+
+        # Add users
+        for i in range(0, num_users):
+            self.add_user(f"User {i + 1}")
+        
+        target_friendships = num_users * avg_friendships
+        total_friendships = 0
+        collisions = 0
+
+        while total_friendships < target_friendships:
+            user_id = random.randint(1, self.last_id)
+            friend_id = random.randint(1, self.last_id)
+
+            if self.add_friendship(user_id, friend_id):
+                total_friendships += 2
+            else:
+                collisions += 1
+        print(f"COLLISIONS: {collisions}")
+
     def get_all_social_paths(self, user_id):
         """
         Takes a user's user_id as an argument
@@ -97,13 +133,23 @@ class SocialGraph:
 
 
 if __name__ == '__main__':
+
+    num_users = 2000
+    avg_friendships = 5
     sg = SocialGraph()
-    sg.populate_graph(1000, 5)
-    connections = sg.get_all_social_paths(1)
+    start_time = time.time()
+    sg.populate_graph(num_users, avg_friendships)
+    end_time = time.time()
+    print(f"Quadratic Runtime: {end_time - start_time} seconds")
+    start_time = time.time()
+    sg.populate_graph_linear(num_users, avg_friendships)
+    end_time = time.time()
+    print(f"Linear Runtime: {end_time - start_time} seconds")
+    # connections = sg.get_all_social_paths(1)
     # print(sg.friendships)
-    print(f"Users in extended social network: {len(connections) - 1}")
-    total_sp = 0
-    for user_id in connections:
-        total_sp += len(connections[user_id])
+    # print(f"Users in extended social network: {len(connections) - 1}")
+    # total_sp = 0
+    # for user_id in connections:
+    #     total_sp += len(connections[user_id])
     
-    print(f"Avg len of social path: {total_sp // len(connections)}")
+    # print(f"Avg len of social path: {total_sp // len(connections)}")
